2c3d3ec65dc114ee4c71425b2139b0659a227ebb
Prasun Mondal <prasun.mondal@oracle.com>
2021-05-22 10:59:51 +0530

    add script details

--- app/src/main/java/com/prasunmondal/postjsontosheets/StringConstants.java
+++ app/src/main/java/com/prasunmondal/postjsontosheets/StringConstants.java
@@ -2,10 +2,12 @@ package com.prasunmondal.postjsontosheets;
 
 public class StringConstants {
     public static String SEPARATOR_DB = "#";
-
+//      Script ID: 1rm7eBju_fdhGl7faifjtMNrmtl43fOM2SdHly3K7kJ5gOwQIV9ZyCZkY
+//      Script Edit: https://script.google.com/home/projects/1rm7eBju_fdhGl7faifjtMNrmtl43fOM2SdHly3K7kJ5gOwQIV9ZyCZkY/edit
 //    public static String DB_SERVER_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwzBamFrEhCDbH1B31N9yfK7XT91UGxBf7-78kAL63fR7u6FBI/exec";
-    public static String DB_SERVER_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyZrdJ_2Qcc_FWZkk6WaFoNlt9Zje5CG3hdPVhkjwcgoWBuKJKNh52Ddg/exec";
+    public static String DB_SERVER_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxUoL9JrUk-wxdl5uDdFHZHzQwFdLHM2Aw-Qompp9t3I75d8oh7CoAoGzFn8F_SPSzWzA/exec";
     public static String DB_SHEET_ID = "1uu_VTWuTpLyjyl_X6VbqdkYkKoR-mkMzk-a4AuYpkjc";
-    public static String DB_TAB_APP_OWNER = "tet";
+    public static String DB_TAB_APP_OWNER = "Sheet2";
 
 }
+
