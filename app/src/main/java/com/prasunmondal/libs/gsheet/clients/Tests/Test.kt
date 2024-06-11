6e8c3167cb3c4c2b0f81e35d87ee581f4c745805
prasunmondal <prasun.mondal02@gmail.com>
2024-06-11 13:30:27 +0530

    enable all tests

--- app/src/main/java/com/prasunmondal/libs/gsheet/clients/Tests/Test.kt
+++ app/src/main/java/com/prasunmondal/libs/gsheet/clients/Tests/Test.kt
@@ -1,5 +1,14 @@
 package com.prasunmondal.libs.gsheet.clients.Tests
 
+import com.prasunmondal.libs.gsheet.clients.Tests.CreateAPIs.InsertObjectTest
+import com.prasunmondal.libs.gsheet.clients.Tests.DeleteAPITests.DeleteAllTest
+import com.prasunmondal.libs.gsheet.clients.Tests.DeleteAPITests.DeleteByAndConditionTest
+import com.prasunmondal.libs.gsheet.clients.Tests.DeleteAPITests.DeleteByOrConditionTest
+import com.prasunmondal.libs.gsheet.clients.Tests.ReadAPIs.CheckData.CheckDataPresenceTest
+import com.prasunmondal.libs.gsheet.clients.Tests.ReadAPIs.FetchData.FetchAllTest
+import com.prasunmondal.libs.gsheet.clients.Tests.ReadAPIs.FetchData.FetchByAndConditionTest
+import com.prasunmondal.libs.gsheet.clients.Tests.ReadAPIs.FetchData.FetchByOrConditionTest
+import com.prasunmondal.libs.gsheet.clients.Tests.ReadAPIs.FetchData.FetchByQueryTest
 import com.prasunmondal.libs.gsheet.clients.Tests.TestBulkOps.TestBulkOps
 
 class Test {
@@ -13,17 +22,17 @@ class Test {
 //            testMultiple()
 
 //        New     New     New     New     New     New     New     New     New
-//            InsertObjectTest()
-////            InsertUniqueObjectTest()
-////            InsertStringArrayTest()
-//            FetchAllTest()
-//            FetchByQueryTest()
-//            FetchByAndConditionTest()
-//            FetchByOrConditionTest()
-//            DeleteAllTest()
-//            DeleteByAndConditionTest()
-//            DeleteByOrConditionTest()
-//            CheckDataPresenceTest()
+            InsertObjectTest()
+//            InsertUniqueObjectTest()
+//            InsertStringArrayTest()
+            FetchAllTest()
+            FetchByQueryTest()
+            FetchByAndConditionTest()
+            FetchByOrConditionTest()
+            DeleteAllTest()
+            DeleteByAndConditionTest()
+            DeleteByOrConditionTest()
+            CheckDataPresenceTest()
 
             TestBulkOps()
 //        }
